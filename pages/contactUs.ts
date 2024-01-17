58118a4def9e10d7ad30c3879da01f6199ae1afd
jeffarat <ingenieroch@icloud.com>
2024-01-16 21:44:09 -0500

    test fixed

--- pages/contactUs.ts
+++ pages/contactUs.ts
@@ -11,14 +11,21 @@ export class ContactUs {
 
     constructor(page: Page){
         this.page = page;
-        this.name = page.locator(`input[placeholder='NAME']`)
+        this.name = page.locator(`input[placeholder='NAME']`);
+        this.lastName = page.locator(`input[placeholder='LAST NAME']`);
+        this.email = page.locator(`input[placeholder='EMAIL']`);
+        this.company = page.locator(`input[placeholder='COMPANY']`);
+        this.message = page.locator(`textarea[placeholder='YOUR MESSAGE']`);
     }
 
     async goto() {
         await this.page.goto('https://www.hexacta.com/contact/');
     }
-    async fillContact(name: string) {
-        await this.name.fill(name);
+    async fillContact(_name: string, _lastName: string, _email: string, _company: string, _message: string) {
+        await this.name.fill(_name);
+        await this.lastName.fill(_lastName);
+        await this.email.fill(_email);
+        await this.company.fill(_company);
+        await this.message.fill(_message);
     }
-
 }
